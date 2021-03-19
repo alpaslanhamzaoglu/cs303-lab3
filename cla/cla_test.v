4570781ccd67fd516b8388c5be75c712a688f579
alpaslanhamzaoglu <44805408+alpaslanhamzaoglu@users.noreply.github.com>
2021-03-19 12:46:08 +0300

    Update cla_test.v

--- cla/cla_test.v
+++ cla/cla_test.v
@@ -1,26 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   13:54:25 12/13/2020
-// Design Name:   CLA20
-// Module Name:   D:/Xilinx/lab3_2/cla_test.v
-// Project Name:  lab3_2
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: CLA20
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
 
 module cla_test;
 
