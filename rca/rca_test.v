634aa938a003dcd8f90ac4d530c8589bf91e7064
alpaslanhamzaoglu <44805408+alpaslanhamzaoglu@users.noreply.github.com>
2021-03-19 12:46:41 +0300

    Update rca_test.v

--- rca/rca_test.v
+++ rca/rca_test.v
@@ -1,26 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   18:14:26 12/12/2020
-// Design Name:   RCA20
-// Module Name:   D:/Xilinx/lab3_1/rca_test.v
-// Project Name:  lab3_1
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: RCA20
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
 
 module rca_test;
 
